67bae1d58c6ae22e92ea7882b78117dd32025dc0
suheda-seyda-sener <t3sesu00@students.oamk.fi>
2024-11-22 14:43:33 +0200

    homepage new carousels

--- src/api/movieFetch.js
+++ src/api/movieFetch.js
@@ -43,7 +43,7 @@ export const fetchRandomMovie = async (genreId) => {
     }
 };
 
-// Update the discoverMovies function to include release_date.gte for unreleased movies
+// Fetch movies for popular movies carousel
 export const discoverMovies = async (params = {}) => {
     try {
         console.log("Discover Movies Params:", params); // Debug log
@@ -63,3 +63,54 @@ export const discoverMovies = async (params = {}) => {
     }
 };
 
+/*
+//export const discoverUpcomingMovies = async (params = {}, pages = [1]) => {
+    try {
+        console.log("Discover Movies Params:", params); // Debug log
+
+        // Initialize an empty array to store results
+        let allMovies = [];
+
+        // Fetch movies from specified pages
+        for (let page of pages) {
+            const response = await axios.get(`${BASE_URL}/discover/movie`, {
+                params: {
+                    api_key: API_KEY,
+                    language: 'en-US',
+                    sort_by: 'release_date.desc', // Sort by release date: latest first
+                    page: page, // Fetch the current page
+                    ...params, // Additional params
+                },
+            });
+
+            console.log(`Discover Movies - Page ${page} Response:`, response.data); // Debug log
+            allMovies = [...allMovies, ...response.data.results]; // Aggregate the results
+        }
+
+        return allMovies; // Return all the aggregated results from all pages
+    } catch (error) {
+        console.error('Error fetching discovered movies:', error);
+        throw error;
+    }
+};
+*/
+
+// Fetch old movies
+export const discoverOldMovies = async (params) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/discover/movie`, {
+            params: {
+                api_key: API_KEY,
+                language: 'en-US',
+                sort_by: 'release_date.asc',  // Sort by release date in ascending order
+                'release_date.gte': new Date().toISOString().split('T')[0], // Filter 
+                ...params, // Additional params if any
+            },
+        });
+        console.log("Old Movies Response:", response.data); // Debug log
+        return response.data.results; // Returns an array of upcoming movies
+    } catch (error) {
+        console.error('Error fetching old movies:', error);
+        throw error;
+    }
+};
--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -3,15 +3,35 @@ import React from 'react';
 import CarouselSelection from '../components/homepage/carouselSelection/CarouselSelection';
 import CarouselSelectionTV from '../components/homepage/carouselSelection/CarouselTV';
 import MoviePicker from '../components/homepage/randomMovie/MoviePicker';
-import { discoverMovies } from '../api/movieFetch';
+import { discoverMovies, discoverOldMovies } from '../api/movieFetch';
 import { topTVSeries } from '../api/tvFetch';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import styles from "../screens/TVSerial.module.css"
 import Navigation from "../components/Navigation"
 const HomePage = () => {
 
-    // Example for fetching popular movies
+    // Fetch popular movies
     const fetchPopularMovies = () => discoverMovies({ sort_by: 'popularity.desc' });
+
+
+    /* // Fetch upcoming movies function
+     const fetchUpcomingMovies = async () => {
+         try {
+             const movies = await discoverUpcomingMovies();
+             console.log("Filtered Upcoming Movies:", movies);
+             return movies;
+         } catch (error) {
+             console.error("Error fetching movies:", error);
+         }
+     };
+     */
+
+
+    // Fetch old movies
+    const fetchOldMovies = () => discoverOldMovies({
+        sort_by: 'release_date.asc',
+        'release_date.gte': new Date().toISOString().split('T')[0],  // Ensures only upcoming movies
+    });
 
     return (
         <div >
@@ -21,15 +41,24 @@ const HomePage = () => {
 
 
             <CarouselSelection
-                title="Discover Popular Movies"
+                title="Popular Movies"
                 fetchMovies={fetchPopularMovies} // Use the discover endpoint
                 viewAllLink="/movies"
             />
 
-
             <div style={{ marginBottom: '30px', marginTop: '30px' }}>
                 <MoviePicker />  {/* Render MoviePicker here */}
             </div>
+
+            <CarouselSelection
+                title="Timeless Movies (1900s)"
+                fetchMovies={fetchOldMovies}
+            />
+
+            {/*<CarouselSelection
+                title="Upcoming Movies"
+                fetchMovies={fetchUpcomingMovies}
+            />*/}
 
             {/* TV Series Carousel */}
             <CarouselSelectionTV
