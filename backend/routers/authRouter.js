5961e620ec1feb2af3ab5d6e46881cd32ed3edd3
suheda-seyda-sener <t3sesu00@students.oamk.fi>
2024-12-06 14:19:10 +0200

    Refactor and add authentication tests and ensure correct functionality

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -15,19 +15,16 @@ router.post("/signup", async (req, res) => {
   }
 
   try {
-    // Check if the email is already registered
     const existingUser = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
     if (existingUser.rows.length > 0) {
       return res.status(400).json({ error: "Email already registered" });
     }
 
-    // Hash password and store user
     const hashedPassword = await bcrypt.hash(password, 10);
     const result = await pool.query(
       "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id",
       [email, hashedPassword]
     );
-
     res.status(201).json({ userId: result.rows[0].id });
   } catch (error) {
     console.error("Error signing up:", error);
--- backend/test_db.sql
+++ backend/test_db.sql
@@ -1,6 +1,8 @@
--- Table: users
+DROP TABLE IF EXISTS favorites;
+DROP TABLE IF EXISTS reviews;
 DROP TABLE IF EXISTS users;
 
+-- Table: users
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email VARCHAR(45) NOT NULL UNIQUE,
@@ -9,8 +11,6 @@ CREATE TABLE users (
 );
 
 -- Table: reviews
-DROP TABLE IF EXISTS reviews;
-
 CREATE TABLE reviews (
   id SERIAL PRIMARY KEY,
   movies_id INT NOT NULL,
@@ -23,10 +23,20 @@ CREATE TABLE reviews (
     REFERENCES users (id)
 );
 
+-- Table: favorites
+CREATE TABLE favorites (
+  id SERIAL PRIMARY KEY,
+  movie_id INT,
+  added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  users_id INT,
+  type VARCHAR(10),
+  title VARCHAR(255),
+  CONSTRAINT fk_favorites_users FOREIGN KEY (users_id)
+    REFERENCES users (id) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+
 -- Unit Testing: Inserting test data for user and review tables to test review browsing feature
-INSERT INTO users (email, password) VALUES ('test@email', '123456');
-INSERT INTO reviews (users_id, movies_id, rating, comment, type) VALUES ('1', '1', '4','test review to get tv series review for a spesific one', 'tv' );
-INSERT INTO reviews (users_id, movies_id, rating, comment, type) VALUES ('1', '1', '4','test review to get tv series review for a spesific one', 'movie' );
--- Zhang Ke add
-INSERT INTO users (email, password) VALUES ('testuser1@example.com', 'password123');
-INSERT INTO users (email, password) VALUES ('testuser2@example.com', 'password456');
+INSERT INTO users (email, password) VALUES ('review@unittest.com', 'test123');
+INSERT INTO reviews (users_id, movies_id, rating, comment, type) VALUES ('1', '1', '4','TV show review - Unit test ', 'tv' );
+INSERT INTO reviews (users_id, movies_id, rating, comment, type) VALUES ('1', '1', '4','Movie review - Unit test', 'movie' );
